e5003ac0bfade105f3155ee7e25d764605c34636
Quinton <qemiller@vt.edu>
2019-03-27 19:48:56 -0400

    messed with some of the rabbitmq stuff

--- capture.py
+++ capture.py
@@ -1,33 +1,41 @@
 import pika
+#import RPi.GPIO as GPIO
+import time
 
 
 
-def getTweet():
+#def getTweet():
     #add code to get tweets from stream in here
     #return an array of thecommand type, place, subject, and message.
     #array should have command type in the zero index, place in the first index,
     #subject in the second, and message in the third.
     
-def waitingForTweet():
+#def waitingForTweet():
     #make the light white
 
-def receivedPublish():
+#def receivedPublish():
     #make the light red
 
-def receivedConsume():
+#def receivedConsume():
     #make the light green
 
-connection = pika.BlockingConnection(pike.ConnectionParameters('localhost'))
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 channel = connection.channel()
+channel.exchange_declare(exchange='Squires', exchange_type='direct')
+channel.exchange_declare(exchange='Goodwin', exchange_type='direct')
+channel.exchange_declare(exchange='Library', exchange_type='direct')
 
 while(1):
-    waitingForTweet()
-    tweet[] = getTweet()
+#    waitingForTweet()
+    tweet = []
+    tweet[0] = input('produce or consume?\n')
+    tweet[1] = input('Place\n')
+    tweet[2] = input('Subject\n')
+    tweet[3] = input('message')
     if tweet[0] == 'p':
         receivedPublish()
-        channel.exchange_declare(exchange=tweet[1], exchange_type='direct')
         channel.basic_publish(exchange=tweet[1], routing_key=tweet[2], body=tweet[3])
-    
+        print('sent ', tweet[3])
     
     if tweet[0] == 'c':
         receivedConsume()
--- repository.py
+++ repository.py
@@ -1,1 +1,23 @@
-######### this is the repo file
+###
+import pika
+
+connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
+inputChannel = connection.channel()
+
+inputChannel.exchange_declare(exchange='Squires',type='direct')
+inputChannel.exchange_declare(exchange='Library',type='direct')
+inputChannel.exchange_declare(exchange='Goodwin',type='direct')
+
+result = inputChannel.queue_declare(exclusive=True)
+queueName = result.method.queue
+exchangeName = result.method.exchange
+
+def callback(ch, method, properties, body): 
+    print(" [x] %r:%r" % (method.routing_key, body))
+
+inputChannel.queue_bind(exchange=exchangeName,queue=queue_name,routing_key=queueName)
+
+print(' [*] Waiting for logs. To exit press CTRL+C')
+
+inputChannel.basic_consume(callback, queue=queue_name, no_ack=True)
+channel.start_consuming()
