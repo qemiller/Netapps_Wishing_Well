da390c5a63228521b1e4c6b99aece239fa384907
cpwhite11 <kalef21@vt.edu>
2019-03-27 20:08:35 -0400

    fixed merge conflicts

--- repository.py
+++ repository.py
@@ -1,6 +1,6 @@
-###
+######### this is the repo file
 import pika
-
+import pymongo
 connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
 inputChannel = connection.channel()
 
@@ -12,7 +12,7 @@ result = inputChannel.queue_declare(exclusive=True)
 queueName = result.method.queue
 exchangeName = result.method.exchange
 
-def callback(ch, method, properties, body): 
+def callback(ch, method, properties, body):
     print(" [x] %r:%r" % (method.routing_key, body))
 
 inputChannel.queue_bind(exchange=exchangeName,queue=queue_name,routing_key=queueName)
@@ -20,4 +20,4 @@ inputChannel.queue_bind(exchange=exchangeName,queue=queue_name,routing_key=queue
 print(' [*] Waiting for logs. To exit press CTRL+C')
 
 inputChannel.basic_consume(callback, queue=queue_name, no_ack=True)
-channel.start_consuming()
+inputChannel.start_consuming()
