091f05178d1bc8e125ff640ea445a2483b8bfdb8
cpwhite11 <35504357+cpwhite11@users.noreply.github.com>
2019-03-31 15:33:24 -0400

    fixed capture py. somehow old comments showed up and stuff

--- capture.py
+++ capture.py
@@ -180,51 +180,8 @@ auth.set_access_token(Access_token, Access_token_secret)
 tweets=Stream(auth, listener())
 
 #reading tweets start with #ECE4564T11
-<<<<<<< HEAD
-#notest there is a space after  #ECE4564T11
 tweets.filter(track=["#ECE4564T11"])
 GPIO.cleanup() #honestly don't know if this will get called when you ctl+c
 
 #soure for ribbitMQ 
 #https://www.vultr.com/docs/how-to-install-rabbitmq-on-ubuntu-16-04-47
-=======
-tweets.filter(track=["#ECE4564T11"])
-
-
-
-
-"""
-#def waitingForTweet():
-    #make the light white
-
-#def receivedPublish():
-    #make the light red
-
-#def receivedConsume():
-    #make the light green
-
-connection = pika.BlockingConnection(pika.ConnectionParameters('localhost'))
-channel = connection.channel()
-channel.exchange_declare(exchange='Squires', exchange_type='direct')
-channel.exchange_declare(exchange='Goodwin', exchange_type='direct')
-channel.exchange_declare(exchange='Library', exchange_type='direct')
-
-while(1):
-#    waitingForTweet()
-    tweet = []
-    tweet[0] = input('produce or consume?\n')
-    tweet[1] = input('Place\n')
-    tweet[2] = input('Subject\n')
-    tweet[3] = input('message')
-    if tweet[0] == 'p':
-        receivedPublish()
-        channel.basic_publish(exchange=tweet[1], routing_key=tweet[2], body=tweet[3])
-        print('sent ', tweet[3])
-    
-    if tweet[0] == 'c':
-        receivedConsume()
-        channel.queue_bind(exchange=tweet[1], queue=queue_name, routing_key=tweet[2])
-        channel.basic_consume(callback, queue=queue_name, no_ack=True)
-        channel.start_consuming()
-"""
->>>>>>> fix the token tweet
